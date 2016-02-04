f0b83b14a7fa63634a8bcfcc0ea007cdfd936edf
Jun Merto <Eufemio Merto@ISD-PF00PJ6D.ph.ibm.com>
2016-02-04 18:00:48 +0800

    Initial commit

---
+++ Sample/src/com/sample/Main.java
@@ -0,0 +1,13 @@
+package com.sample;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		displayGreeting();
+	}
+
+	private static void displayGreeting() {
+		System.out.println("Hello World!");
+	}
+}
