5185e16ebe3a6049c139351d7d8489be7f8c7422
Jun Merto <Eufemio Merto@ISD-PF00PJ6D.ph.ibm.com>
2016-02-04 18:36:19 +0800

    removed output message

--- Sample/src/com/sample/Main.java
+++ Sample/src/com/sample/Main.java
@@ -3,17 +3,9 @@ package com.sample;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		displayGreeting();
-		
 		Calculator calc = new Addition();
 		Calculator calc2 = new Subtract();
 		System.out.println(calc.calculate(1, 2));
 		System.out.println(calc2.calculate(1, 2));
-	}
-
-	private static void displayGreeting() {
-		System.out.println("Hello World!");
-		System.out.println("Hello Universe!");
 	}
 }
