74ca673528ceda69d790f47fed6ce4899c52c992
Jun Merto <Eufemio Merto@ISD-PF00PJ6D.ph.ibm.com>
2016-02-05 15:05:07 +0800

    Added new computation

--- Sample/src/com/sample/Main.java
+++ Sample/src/com/sample/Main.java
@@ -4,8 +4,12 @@ public class Main {
 
 	public static void main(String[] args) {
 		Calculator calc = new Addition();
-		System.out.println(calc.calculate(1, 2));
+		System.out.println(calc.calculate(1.3, 2));
 		calc = new Subtraction();
+		System.out.println(calc.calculate(1, 2));
+		calc = new Multiplication();
+		System.out.println(calc.calculate(1, 2));
+		calc = new Division();
 		System.out.println(calc.calculate(1, 2));
 	}
 }
