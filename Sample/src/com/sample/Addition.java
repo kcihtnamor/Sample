c31c0a47cb583c7f71226675207868d927be03bf
Jun Merto <Eufemio Merto@ISD-PF00PJ6D.ph.ibm.com>
2016-02-05 14:44:06 +0800

    Added logic to check the type of number

--- Sample/src/com/sample/Addition.java
+++ Sample/src/com/sample/Addition.java
@@ -5,7 +5,11 @@ public class Addition implements Calculator {
 	@Override
 	public Number calculate(Number a, Number b) {
 		Number result = 0;
-		result = a.doubleValue() + b.doubleValue();
+		if(a instanceof Double) {
+			result = a.doubleValue() + b.doubleValue();
+		}else if(a instanceof Integer) {
+			result = a.intValue() + b.intValue();
+		}
 		return result;
 	}
 
