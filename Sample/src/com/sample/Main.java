c6a8bcd174ea9fdce83b5d71119e5db7a5b7d73d
Jun Merto <Eufemio Merto@ISD-PF00PJ6D.ph.ibm.com>
2016-02-05 14:49:22 +0800

    Remove other Calculator object.

--- Sample/src/com/sample/Main.java
+++ Sample/src/com/sample/Main.java
@@ -4,8 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		Calculator calc = new Addition();
-		Calculator calc2 = new Subtract();
 		System.out.println(calc.calculate(1, 2));
-		System.out.println(calc2.calculate(1, 2));
+		calc = new Subtract();
+		System.out.println(calc.calculate(1, 2));
 	}
 }
